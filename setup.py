c30222d173ab3e6d869d348dd44a69f426b93294
MKN TG <86054899+MrMKN@users.noreply.github.com>
2023-01-30 10:25:34 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setuptools.setup(
         "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
         "Operating System :: OS Independent",
     ],
-    python_requires='~=3.7',
+    python_requires=">=3.6",
+    install_requires=requirements()
     py_modules=["pyleaves"],
 )
