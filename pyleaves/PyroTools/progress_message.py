61bc99a450922100afa8698b06f16fef6801312a
MKN TG <86054899+MrMKN@users.noreply.github.com>
2023-02-02 21:15:13 +0530

    Create progress_message.py

---
+++ pyleaves/PyroTools/progress_message.py
@@ -0,0 +1,56 @@
+# Copyright (©) 2023 https://github.com/MrMKN
+# Telegram Link : https://t.me/Mr_MKN 
+# Repo Link : https://github.com/MrMKN/PyLeaves
+# License Link : https://github.com/MrMKN/PyLeaves/blob/main/LICENSE
+
+import math, time
+from pyleaves.utils import *
+
+
+
+async def progress_for_pyrogram(
+    current,
+    total,
+    ud_type,
+    message,
+    start,
+    template: str,    
+    finished_str='●',
+    unfinished_str='○',
+    markup=None,
+):
+
+    now = time.time()
+    diff = now - start
+    if round(diff % 10.00) == 0 or current == total:
+        percentage = current * 100 / total
+        speed = current / diff
+        elapsed_time = round(diff) * 1000
+        time_to_completion = round((total - current) / speed) * 1000
+        estimated_total_time = elapsed_time + time_to_completion
+
+        elapsed_time = TimeFormatter(milliseconds=elapsed_time)
+        estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
+
+
+        progress = "{0}{1}".format(
+            ''.join([finished_str for i in range(math.floor(percentage / 5))]),
+            ''.join([unfinished_str for i in range(20 - math.floor(percentage / 5))]))
+          
+        Template = template if template else txt.TEMPLATE 
+        tmp = progress + Template.format( 
+            round(percentage, 2),
+            humanbytes(current),
+            humanbytes(total),
+            humanbytes(speed),
+            estimated_total_time if estimated_total_time != '' else "0 s"
+        )
+        try:
+            await message.edit(text=f"{ud_type}\n\n{temp}", reply_markup=markup)                       
+        except Exception as e:
+            print(e)
+
+
+
+
+
