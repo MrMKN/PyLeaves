4741a16cb1ce3077df576fc1d00d71f256812368
MKN TG <86054899+MrMKN@users.noreply.github.com>
2023-02-03 07:44:30 +0530

    Update __init__.py

--- pyleaves/PyroTools/__init__.py
+++ pyleaves/PyroTools/__init__.py
@@ -1,1 +1,7 @@
+# Copyright (©) 2023 https://github.com/MrMKN
+# Telegram Link : https://t.me/Mr_MKN 
+# Repo Link : https://github.com/MrMKN/PyLeaves
+# License Link : https://github.com/MrMKN/PyLeaves/blob/main/LICENSE
+
+
 from .progress_message import pyro_progress        
