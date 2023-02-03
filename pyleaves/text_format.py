09fc23f4fdce9e01800bd905582090e270be5a91
MKN TG <86054899+MrMKN@users.noreply.github.com>
2023-02-03 07:43:54 +0530

    Update text_format.py

--- pyleaves/text_format.py
+++ pyleaves/text_format.py
@@ -1,3 +1,8 @@
+# Copyright (©) 2023 https://github.com/MrMKN
+# Telegram Link : https://t.me/Mr_MKN 
+# Repo Link : https://github.com/MrMKN/PyLeaves
+# License Link : https://github.com/MrMKN/PyLeaves/blob/main/LICENSE
+
 
 
 #example template 
