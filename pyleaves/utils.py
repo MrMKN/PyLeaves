4ebf5bdc862facf8dfaae6ba1cf5512e377f2b2f
MKN TG <86054899+MrMKN@users.noreply.github.com>
2023-02-03 07:44:12 +0530

    Update utils.py

--- pyleaves/utils.py
+++ pyleaves/utils.py
@@ -1,3 +1,9 @@
+# Copyright (©) 2023 https://github.com/MrMKN
+# Telegram Link : https://t.me/Mr_MKN 
+# Repo Link : https://github.com/MrMKN/PyLeaves
+# License Link : https://github.com/MrMKN/PyLeaves/blob/main/LICENSE
+
+
 import math, time
 
 
