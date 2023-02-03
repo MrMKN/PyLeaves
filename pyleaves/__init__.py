ad2342c715b5afc59b2b78f275f3ddb6d065732f
MKN TG <86054899+MrMKN@users.noreply.github.com>
2023-02-03 07:43:14 +0530

    copyright 2023

--- pyleaves/__init__.py
+++ pyleaves/__init__.py
@@ -1,4 +1,7 @@
-# © https://github.com/MrMKN
+# Copyright (©) 2023 https://github.com/MrMKN
+# Telegram Link : https://t.me/Mr_MKN 
+# Repo Link : https://github.com/MrMKN/PyLeaves
+# License Link : https://github.com/MrMKN/PyLeaves/blob/main/LICENSE
 
 
 from pyleaves.leaves import Leaves
